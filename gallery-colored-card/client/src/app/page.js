6e2ad35346055f70f28b24c6f53cf4e76ac8a524
equaa <1unluckyss1@gmail.com>
2023-10-15 23:48:35 +0300

    feat: add 2 projects

---
+++ gallery-colored-card/client/src/app/page.js
@@ -0,0 +1,72 @@
+"use client";
+import styles from "./page.module.css";
+import gsap from "gsap";
+
+const projects = [
+  {
+    title: "New York",
+    color: "#F06318",
+  },
+  {
+    title: "San Francisco",
+    color: "#DCF018",
+  },
+  {
+    title: "San Antonio",
+    color: "#18F0E8",
+  },
+  {
+    title: "Nashville",
+    color: "#8C0CF0",
+  },
+  {
+    title: "Houston",
+    color: "#F0183C",
+  },
+  {
+    title: "New Orleans",
+    color: "#F0BA18",
+  },
+  {
+    title: "Oklahoma City",
+    color: "#0C34F0",
+  },
+  {
+    title: "Detroit",
+    color: "#0CBCF0",
+  },
+  {
+    title: "Indianapolis",
+    color: "#91F018",
+  },
+];
+
+export default function Home() {
+  const handleMouseEnter = (e, color) => {
+    gsap.to(e.target, { backgroundColor: color, top: "-2vw", duration: 0.3 });
+  };
+
+  const handleMouseLeave = (e, color) => {
+    gsap.to(e.target, { backgroundColor: "white", top: "0vw", duration: 0.3 });
+  };
+
+  return (
+    <main className={styles.main}>
+      {projects.map((project, index) => {
+        return (
+          <div
+            onMouseEnter={(e) => {
+              handleMouseEnter(e, project.color);
+            }}
+            onMouseLeave={(e) => {
+              handleMouseLeave(e, project.color);
+            }}
+            key={index}
+          >
+            <p>{project.title}</p>
+          </div>
+        );
+      })}
+    </main>
+  );
+}
---
+++ infinite-text-move-scroll/src/app/page.js
@@ -0,0 +1,58 @@
+"use client";
+import Image from "next/image";
+import styles from "./page.module.css";
+import gsap from "gsap";
+import { useEffect, useRef } from "react";
+import { ScrollTrigger } from "gsap/all";
+
+export default function Home() {
+  const firstText = useRef(null);
+  const secondText = useRef(null);
+  const slider = useRef(null);
+  let xPercent = 0;
+  let direction = -1;
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    requestAnimationFrame(animation);
+
+    gsap.to(slider.current, {
+      scrollTrigger: {
+        trigger: document.documentElement,
+        start: 0,
+        end: window.innerHeight,
+        scrub: true,
+        onUpdate: (e) => (direction = e.direction * -1),
+      },
+      x: "-=300px",
+    });
+  }, []);
+
+  const animation = () => {
+    if (xPercent <= -103) {
+      xPercent = 0;
+    }
+    if (xPercent > 0) {
+      xPercent = -103;
+    }
+    // if (xPercent > 0) {
+    //   xPercent = -103;
+    // }
+    gsap.set(firstText.current, { xPercent: xPercent });
+    gsap.set(secondText.current, { xPercent: xPercent });
+    xPercent += 0.05 * direction;
+    requestAnimationFrame(animation);
+  };
+
+  return (
+    <main className={styles.main}>
+      <Image fill={true} src="/images/background.jpg" alt="image" />
+      <div className={styles.sliderContainer}>
+        <div ref={slider} className={styles.slider}>
+          <p ref={firstText}>Infinite text move on scroll </p>
+          <p ref={secondText}>Infinite text move on scroll </p>
+        </div>
+      </div>
+    </main>
+  );
+}
